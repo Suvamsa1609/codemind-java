ac013ec5c2f7fa7000b334dc5fa2ff41d31274c8
Suvamsa1609 <96298524+Suvamsa1609@users.noreply.github.com>
2022-07-02 22:05:53 +0530

    import java.util.Scanner; class Sample {     public static void main(String args[])     {         int n,i,x[],count=0;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)          x[i]=sc.nextInt();
            for(i=1;i<n-1;i++)
            {
                if(x[i-1]%2==0 &&  x[i+1]%2==0)
                {
                    if(x[i]%2==0)
                        count++;
                }
            }
            System.out.println(count);
             
        }
    }

---
+++ count_even_numbers_between_even_numbers.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class Sample
+{
+    public static void main(String args[])
+    {
+        int n,i,x[],count=0;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        x=new int[n];
+        for(i=0;i<n;i++)
+         x[i]=sc.nextInt();
+        
+        for(i=1;i<n-1;i++)
+        {
+            if(x[i-1]%2==0 &&  x[i+1]%2==0)
+            {
+                if(x[i]%2==0)
+                    count++;
+            }
+        }
+        System.out.println(count);
+         
+    }
+}
